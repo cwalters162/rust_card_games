338d976c972ecb4e478eec832d8072534c2b641e
Cherokee Walters <cherokeewalters162@gmail.com>
2023-09-20 14:16:51 -0400

    Pulled the deck/card functions into it's own module

---
+++ src/deck.rs
@@ -0,0 +1,61 @@
+use std::fmt;
+use rand::prelude::*;
+
+#[derive(Clone, Debug)]
+pub struct Card {
+    name: String,
+    suit: String,
+    value: usize
+}
+
+impl Card {
+    fn what_card(&self) {
+        println!("I have the {} of {} and it's value is {}", self.name, self.suit, self.value)
+    }
+
+    pub fn get_value(&self) -> usize {
+        self.value
+    }
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "The {} of {}", self.name, self.suit)
+    }
+}
+
+fn shuffle(vec: &mut Vec<Card>) {
+    let mut rng = rand::thread_rng();
+    vec.shuffle(&mut rng);
+}
+
+pub fn make_deck(isShuffled: bool) -> Vec<Card> {
+    let suits = Vec::from(["Diamonds", "Hearts", "Clubs", "Spades"]);
+    let names = Vec::from(["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]);
+    let values = Vec::from([1,2,3,4,5,6,7,8,9,10,11,12,13]);
+
+    let mut cards = Vec::new();
+
+    for suit in suits {
+        for (x, _value) in values.iter().enumerate() {
+            cards.push(Card {
+                name: names[x].to_string(),
+                suit: suit.to_string(),
+                value: values[x]
+            })
+        }
+    }
+    if isShuffled == true {
+        shuffle(&mut cards);
+    }
+    cards
+}
+
+fn test() {
+    let cards = make_deck(true);
+
+    for card in cards {
+        card.what_card();
+        println!("Current Card {}", card);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,16 @@
+mod clock;
+mod deck;
+
+use crate::deck::make_deck;
+use crate::deck::Card;
+
 use std::collections::VecDeque;
 use rand;
 use rand::seq::SliceRandom;
-use std::fmt;
 use std::ops::IndexMut;
 use std::time::{Duration, Instant};
 
-#[derive(Clone, Debug)]
-struct Card {
-    name: String,
-    suit: String,
-    value: usize
-}
 
-impl Card {
-    fn what_card(&self) {
-        println!("I have the {} of {} and it's value is {}", self.name, self.suit, self.value)
-    }
-}
-
-impl fmt::Display for Card {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "The {} of {}", self.name, self.suit)
-    }
-}
-
-fn shuffle(vec: &mut Vec<Card>) {
-    let mut rng = rand::thread_rng();
-    vec.shuffle(&mut rng);
-}
-
-fn make_cards(isShuffled: bool) -> Vec<Card> {
-    let suits = Vec::from(["Diamonds", "Hearts", "Clubs", "Spades"]);
-    let names = Vec::from(["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]);
-    let values = Vec::from([1,2,3,4,5,6,7,8,9,10,11,12,13]);
-
-    let mut cards = Vec::new();
-
-    for suit in suits {
-        for (x, _value) in values.iter().enumerate() {
-            cards.push(Card {
-                name: names[x].to_string(),
-                suit: suit.to_string(),
-                value: values[x]
-            })
-        }
-    }
-    if isShuffled == true {
-        shuffle(&mut cards);
-    }
-    cards
-}
-
-fn test() {
-    let cards = make_cards(true);
-
-    for card in cards {
-        card.what_card();
-        println!("Current Card {}", card);
-    }
-}
 
 #[derive(PartialEq)]
 enum GameResult {
@@ -77,7 +29,7 @@ fn clock() -> GameResult {
     let mut won_or_loss = GameResult::WON;
     let time_to_view_cards = Duration::from_secs(2);
     let mut game_complete = false;
-    let mut cards = make_cards(true);
+    let mut cards = make_deck(true);
     let mut split_cards: Vec<VecDeque<Card>> = Vec::new();
 
     for _ in 0..13 {
@@ -108,10 +60,10 @@ fn clock() -> GameResult {
         round += 1;
         //println!("Round {}. Current Card: {}", &round, &current_card);
 
-        let current_card_value = current_card.value;
+        let current_card_value = current_card.get_value();
         split_cards.index_mut(current_card_value - 1).push_front(current_card);
         current_card = split_cards.index_mut(current_card_value - 1).pop_back().unwrap();
-        if current_card.value == 13 {
+        if current_card.get_value() == 13 {
             times_seen_king += 1
         }
 
@@ -121,7 +73,7 @@ fn clock() -> GameResult {
 
             for (i, pile) in split_cards.iter().enumerate() {
                 for card in pile.iter() {
-                    if card.value - 1 != i {
+                    if card.get_value() - 1 != i {
                         won_or_loss = GameResult::LOSS;
                     }
                 }
@@ -138,7 +90,7 @@ fn main() {
     let mut games_won: f64 = 0.0;
     let mut games_loss: f64 = 0.0;
 
-    for _ in 0..10000000 {
+    for _ in 0..1000000 {
         match clock() {
             GameResult::WON => games_won += 1.0,
             GameResult::LOSS => games_loss += 1.0,
